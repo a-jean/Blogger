c0b5b94e0ee31957b011caf3e5b50a97beec8e8b
Ashley Jean <ashh.jean@gmail.com>
2016-03-13 13:36:12 -0400

    Created NEW action and view, created a form for article

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Blogger::Application.routes.draw do
   resources :articles
-end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+root 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -55,3 +55,4 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+end
