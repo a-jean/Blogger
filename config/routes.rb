d79cc169aa82d27adfb196f4d4838515cb40c2f8
Ashley Jean <ashh.jean@gmail.com>
2016-04-11 22:01:02 -0400

    Installed sorcery gem, used scaffold generator, left off visiting authors page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Blogger::Application.routes.draw do
 
   end
     resources :tags
+    resources :authors 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
